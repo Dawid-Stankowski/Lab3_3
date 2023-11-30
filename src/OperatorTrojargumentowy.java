23c8143817b8eda85040bd688ebab0b7a733a81f
Dawid-Stankowski <dawid441@interia.pl>
2023-11-30 13:15:18 +0100

    1.0

--- src/OperatorTrojargumentowy.java
+++ src/OperatorTrojargumentowy.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class OperatorTrojargumentowy {
+public class OperatorTrojargumentowy {
+    public static void main(String[] args) {
+        // Przykładowe zmienne
+        int liczba1 = 10;
+        int liczba2 = 20;
+
+        // Użycie operatora trójargumentowego do porównania liczb
+        String wynikPorownania = (liczba1 > liczba2) ? "Liczba1 jest większa" : "Liczba2 jest większa lub równa";
+
+        // Wyświetlenie wyniku porównania
+        System.out.println(wynikPorownania);
+
+        // Przykładowe użycie operatora trójargumentowego dla różnych typów
+        int x = 5;
+        int y = 8;
+        String wynik = (x > y) ? "x jest większe od y" : "x nie jest większe od y";
+
+        // Wyświetlenie wyniku
+        System.out.println(wynik);
+    }
 }
